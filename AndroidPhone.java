d8df9db7cf1b9d2125babab2d0ebc44873b12647
Prahlad Kumar Popuri <prahladkumar2222@gmail.com>
2018-10-03 11:34:28 +0530

    Update AndroidPhone.java

--- AndroidPhone.java
+++ AndroidPhone.java
@@ -5,17 +5,23 @@ public class AndroidPhone {
 	int phonePrice=21980,screenSize=6;
 	double thickness=0.32;
 	boolean isAudioJackPresent=true;
+	
+	
+	
     String phoneName="Redmi Note 5 Pro";
 	String phoneBrand="Xiaomi";
 	
 	public void myAwesomeProjects() {
 		
-		System.out.println("The phone price is "+phonePrice);
+		System.out.println("The phone price is "+ phonePrice);
 		System.out.println("The screenSize is "+screenSize+"inches");
-		System.out.println("The thickness of the phone is "+thickness+"inches");
+		System.out.println("The thickness of the phone is "+ thickness+"inches");
 		System.out.println("The condition of AudioJack is "+isAudioJackPresent);
 		System.out.println("The name of the mobile is "+phoneName);
 		System.out.println("The name of the company manufacturing mobile is "+phoneBrand);
+		
+		
+		
 		
 	}
 	
@@ -27,7 +33,7 @@ public class AndroidPhone {
 	    this.isAudioJackPresent=isAudioJackPresent;
 	    this.phoneName=phoneName;
 	    this.phoneBrand=phoneBrand;
-	}
+	}//thankyou bhaiyaa
 	
 	
 }
